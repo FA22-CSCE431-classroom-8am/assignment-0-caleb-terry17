a5f3cb3f119bd5b027d7285b701ed99dc181ac69
caleb-terry17 <k.cterry30@gmail.com>
2022-09-15 20:39:50 -0500

    updated unit tests to include testing for negative prices

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ApplicationRecord
     validates :title, presence: true
     validates :author, presence: true
     validates :published, presence: true
-    validates :price, presence: true
+    validates :price, presence: true, numericality: {greater_than_or_equal_to: 0} 
 end
